657364f15aa4d7712888b00edbebef46f6eece94
andreypopov1803 <122984785+andreypopov1803@users.noreply.github.com>
2023-01-24 16:36:25 +0300

    Add files via upload

---
+++ task10.cs
@@ -0,0 +1,14 @@
+﻿/*Напишите программу, которая на вход принимает трехзначное 
+число и на выходе показывает вторую цифру этого числа*/
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int num1, num2, num3;
+Console.WriteLine(num);
+num3 = num % 10;
+num/=10;
+num2 = num % 10;
+num /= 10;
+num1 = num % 10;
+num /= 10;
+
+Console.WriteLine($"Вторая цифра введенного числа это {num2}");
---
+++ task13.cs
@@ -0,0 +1,20 @@
+﻿/*Напишите программу, которая выводит третью цифру 
+заданного числа или сообщает, что третьей цифры нет*/
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int num1, num2, num3;
+Console.WriteLine(num);
+if (num>99)
+{
+    num3 = num % 10;
+    num/=10;
+    num2 = num % 10;
+    num /= 10;
+    num1 = num % 10;
+    num /= 10;
+    Console.WriteLine($"Третья цифра введенного числа равна {num3}");
+}else{
+    Console.WriteLine("Третьей цифры нет");
+}
+
+
