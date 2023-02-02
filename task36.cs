345ffa564b42d18a2f27e963a4250e6fb27f82df
andreypopov1803 <122984785+andreypopov1803@users.noreply.github.com>
2023-02-02 23:18:53 +0300

    Add files via upload

---
+++ task36.cs
@@ -0,0 +1,18 @@
+﻿/*Задача 36. Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, 
+стоящих на нечетных позициях*/
+//Console.Write("Введите количество элементов массива: ");
+//int number = Convert.ToInt32(Console.ReadLine());
+int[] array = new int [6];
+int sum = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(1, 5);
+    Console.Write($"{array[i]}, ");
+    if (i % 2 == 0)
+    {
+        sum = sum + array[i];
+    }
+}
+
+
+Console.WriteLine("Сумма элементов, стоящих на нечетных позициях равна:{0}", sum);
---
+++ task38.cs
@@ -0,0 +1,16 @@
+﻿/*38.Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным 
+элементами массива*/
+double[] array = new double [6];
+double max = 0;
+double min = 1000;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(1, 1000);
+    Console.Write($"{array[i]}, ");
+    if (array[i] < min) min = array[i];
+    if (array[i] > max) max = array[i];
+}
+double dif = max - min;
+Console.WriteLine("Максимальное значение массива равно:{0}", max);
+Console.WriteLine("Минимальное значение массива равно:{0}", min);
+Console.WriteLine("Разница между максимальным и минимальным значениями массива равно:{0}", dif);
