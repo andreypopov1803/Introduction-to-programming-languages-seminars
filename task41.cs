fcdefbbbb32db9ce8db8b2ba4e7cef2e06d8483d
andreypopov1803 <122984785+andreypopov1803@users.noreply.github.com>
2023-02-08 21:22:09 +0300

    Add files via upload

---
+++ task41.cs
@@ -0,0 +1,12 @@
+﻿/*Задача 41. Пользователь вводит с клавиатуры М чисел. Посчитайте сколько чисел больше 0 ввел польз.*/
+int count = 0;
+for (int i = 0; i < 10; i++)
+{
+    Console.Write("Введите число N: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    if (num > 0)
+    {
+        count = count +1;
+    }
+}
+Console.Write(count);
---
+++ task43.cs
@@ -0,0 +1,14 @@
+﻿/*Напишите программу, которая найдет точку пересечения двух прямых, заданных уравнениями
+y = k1 * x + b1 и y = k2 * x + b2 */
+Console.Write("Введите число b1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число k2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double x = (b2 - b1) / (k1 - k2);
+double y = k2 * x + b2;
+Console.WriteLine($"Координаты точки пересечения двух прямых:({x};{y})");
